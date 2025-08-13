dcbce0036474432808dda0a5f7e5fc952e23b589
ErragotiAsha <ashasekhar.2004@gmail.com>
2025-08-13 16:07:06 +0530

    Update backend.tf

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "asha"      
-    storage_account_name = "ashaa"      
-    container_name       = "ashaaa"                 
+    storage_account_name = "ashaaa"      
+    container_name       = "ashaaaa"              
     key                  = "k8s.tfstate"  
   }
 }
